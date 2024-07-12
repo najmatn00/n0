b908edf8224f59ba4314ccf0ebc40b8871959eb0
najmehtn00 <najmehtn1379@gmail.com>
2024-07-12 14:32:12 +0330

    k

---
+++ app/Login.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+const Login = () => {
+  return (
+    <View>
+      <Text style={styles.cont}>Login kon</Text>
+    </View>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+  cont: {
+    color:"blue",
+  }
+})
