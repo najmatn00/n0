ca8841be7b58e0aa4ec777d966825ba63193da4c
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-07-12 15:28:00 +0330

    fixed tailwind

---
+++ app/(tabs)/Login.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+const Login = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-xl ">Login kon</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Login
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the app folder
+    "./components/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the components folder]
+    "./app/(tabs)/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rmono: ["Roboto-Mono", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
